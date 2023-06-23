d37aea84dfc37fbd6922154afc2bc0c078fbc265
Amine Ramadane <Amineramadane98@gmail.com>
2023-06-23 16:16:20 +0100

    update function 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include "holberton.h"
 
-int _isupper(int c)
-
+int _isupper(int x)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (x >= 65 && x <= 90)
+	{
 		return (1);
-	else
-		return (0);
+	}
 
+	return (0);
 }
